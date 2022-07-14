5d8f07e27cfc7a50f6cde9c7f756dafb64abcdf0
Astatine-fever <108997067+Astatine-fever@users.noreply.github.com>
2022-07-15 00:46:43 +0530

    Add files via upload

---
+++ HTML/JavaScript/adda.js
@@ -0,0 +1,10 @@
+function adda() {
+    let x, y, z; // Statement 1
+  //  x = 6; // Statement 2
+   // y = 5; // Statement 3
+   x= 5;
+   y= 6;
+   z = x + y;
+    document.getElementById("demo").innerHTML = `The value of z is ${z}.`;
+
+}
---
+++ HTML/JavaScript/inner_html.js
@@ -0,0 +1,14 @@
+function sample()
+{   
+document.getElementById("demo").innerHTML = 5 + 6;
+}
+
+function writer()
+{
+    document.write(5+5);
+}
+
+function alert1()
+{
+    window.alert(5 + 6);
+}
