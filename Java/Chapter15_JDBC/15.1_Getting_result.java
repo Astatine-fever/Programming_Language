f7d3564c76da41395eece97060601599110c4321
Prasanna <smarterthannewton@gmail.com>
2022-07-16 01:00:50 +0530

    JDBC_Updated

--- Java/Chapter15_JDBC/15.1_Getting_result.java
+++ Java/Chapter15_JDBC/15.1_Getting_result.java
@@ -8,16 +8,23 @@ import java.sql.*;
 			// This is to check whether the database is connected... using try catch
 		try // This will try the statements 
 		{
-			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3310/JDBC1?allowMultiQueries=true","root","Sytem@123");
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3309/?allowMultiQueries=true","root","");
 			Class.forName("com.mysql.cj.jdbc.Driver");	
-			PreparedStatement ps = con.prepareStatement("Select * from siva");//Insert into astatine values(2314,'Harishankar');
-			//ps.executeUpdate();
-			System.out.println("Table Created");
-			ResultSet rs = ps.executeQuery();
+			
+			PreparedStatement ps0; 
+			ps0=con.prepareStatement("use office;");
+			ps0.executeUpdate();
+			System.out.println("Database selected");
+
+			ps0 = con.prepareStatement("Select * from employee_list;");
+			ps0.executeQuery();
+			System.out.println("Table Selected");
+			
+			ResultSet rs = ps0.executeQuery();
 			while(rs.next())//rs.next is used to get values continously till last row  
 			{
 				
-				System.out.println(rs.getInt(1)+" "+rs.getString(2));
+				System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
 		} 
 		}
 		catch (Exception e) // This will catch any exception during try session 
